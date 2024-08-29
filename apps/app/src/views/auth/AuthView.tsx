f15bc72070b5d6a4412ae04f4b6aabb9ec12f6bd
jennmueng <ldrwarlock@gmail.com>
2024-08-28 17:05:43 -0700

    fix auth screen on desktop and centering

--- apps/app/src/views/auth/AuthView.tsx
+++ apps/app/src/views/auth/AuthView.tsx
@@ -15,39 +15,41 @@ export const AuthView = () => {
 
 	return (
 		<SimpleLayout>
-			<div className="px-8 flex w-full max-w-md flex-col items-center justify-center py-8">
-				<div className="flex flex-col items-center justify-center gap-4">
-					<img src="/logo.png" className="w-24" alt="Cloudy" />
-					<div className="text-2xl font-bold font-display text-center">Supercharge your thoughts.</div>
-					<div className="text-lg font-medium text-secondary text-center">Sign in to Cloudy</div>
-				</div>
-				<div className="w-full">
-					<Auth
-						supabaseClient={supabase}
-						providers={["google"]}
-						appearance={{
-							theme: ThemeSupa,
-							variables: {
-								default: {
-									colors: {
-										brand: "rgb(var(--color-accent))",
-										brandAccent: "rgb(var(--color-accent) / 0.8)",
-									},
-									radii: {
-										borderRadiusButton: "0.25rem",
+			<div className="flex w-full h-dvh flex-col items-center justify-center p-8 overflow-y-scroll">
+				<div className="max-w-md">
+					<div className="flex flex-col items-center justify-center gap-4">
+						<img src="/logo.png" className="w-24" alt="Cloudy" />
+						<div className="text-2xl font-bold font-display text-center">Supercharge your thoughts.</div>
+						<div className="text-lg font-medium text-secondary text-center">Sign in to Cloudy</div>
+					</div>
+					<div className="w-full">
+						<Auth
+							supabaseClient={supabase}
+							providers={["google"]}
+							appearance={{
+								theme: ThemeSupa,
+								variables: {
+									default: {
+										colors: {
+											brand: "rgb(var(--color-accent))",
+											brandAccent: "rgb(var(--color-accent) / 0.8)",
+										},
+										radii: {
+											borderRadiusButton: "0.25rem",
+										},
 									},
 								},
-							},
-							className: {
-								container: "font-sans",
-								// button: "bg-accent text-background border-none font-sans font-medium rounded-md hover:bg-red-500",
-								label: "font-sans font-medium",
-								input: "font-sans rounded",
-								message: "font-sans",
-								anchor: "font-sans",
-							},
-						}}
-					/>
+								className: {
+									container: "font-sans",
+									// button: "bg-accent text-background border-none font-sans font-medium rounded-md hover:bg-red-500",
+									label: "font-sans font-medium",
+									input: "font-sans rounded",
+									message: "font-sans",
+									anchor: "font-sans",
+								},
+							}}
+						/>
+					</div>
 				</div>
 			</div>
 		</SimpleLayout>
