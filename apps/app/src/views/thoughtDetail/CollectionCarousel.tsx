26a966e22008d926bb4d2b88b2be72005739beab
jennmueng <ldrwarlock@gmail.com>
2024-09-01 14:22:08 -0700

    collection add on new

--- apps/app/src/views/thoughtDetail/CollectionCarousel.tsx
+++ apps/app/src/views/thoughtDetail/CollectionCarousel.tsx
@@ -1,7 +1,8 @@
+import { handleSupabaseError } from "@cloudy/utils/common";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { ListIcon, PlusIcon, SparklesIcon } from "lucide-react";
 import { useMemo } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useUpdateEffect } from "react-use";
 
 import { supabase } from "src/clients/supabase";
@@ -20,6 +21,8 @@ import { Chip } from "src/components/Chip";
 import { CollectionDropdown } from "src/components/CollectionDropdown";
 import { useSuggestedCollectionsStore } from "src/stores/suggestedCollection";
 import { useUser } from "src/stores/user";
+
+import { useEditThought } from "./hooks";
 
 interface Collection {
 	id: string;
@@ -47,36 +50,64 @@ const useCollections = () => {
 };
 
 const useNewCollection = () => {
+	const { mutateAsync: editThought } = useEditThought();
+
+	const navigate = useNavigate();
+
 	return useMutation({
-		mutationFn: async (payload: { title: string; thoughtId: string }) => {
-			const { data, error } = await supabase
-				.from("collections")
-				.insert({
-					title: payload.title,
-				})
-				.select();
+		mutationFn: async (payload: { title: string; thoughtId?: string | null }) => {
+			let thoughtIdToUse = payload.thoughtId;
 
-			if (error) {
-				throw error;
+			if (!thoughtIdToUse) {
+				const thought = await editThought({});
+				thoughtIdToUse = thought?.id;
+				if (thought?.id) {
+					navigate(`/thoughts/${thought.id}`, { replace: true, preventScrollReset: true });
+				}
 			}
 
-			if (!data) {
-				throw new Error("No data returned from creating collection");
+			if (!thoughtIdToUse) {
+				return;
 			}
+
+			const newCollection = handleSupabaseError(
+				await supabase
+					.from("collections")
+					.insert({
+						title: payload.title,
+					})
+					.select()
+					.single(),
+			);
 
 			await supabase.from("collection_thoughts").insert({
-				collection_id: data[0].id,
-				thought_id: payload.thoughtId,
+				collection_id: newCollection.id,
+				thought_id: thoughtIdToUse,
 			});
 
-			return data;
+			return newCollection;
 		},
 	});
 };
 
 const useAddToCollection = () => {
+	const { mutateAsync: editThought } = useEditThought();
+
+	const navigate = useNavigate();
+
 	return useMutation({
-		mutationFn: async (payload: { collectionId: string; thoughtId: string }) => {
+		mutationFn: async (payload: { collectionId: string; thoughtId?: string | null }) => {
+			if (!payload.thoughtId) {
+				const thought = await editThought({});
+				payload.thoughtId = thought?.id;
+				if (thought?.id) {
+					navigate(`/thoughts/${thought.id}`, { replace: true, preventScrollReset: true });
+				}
+			}
+			if (!payload.thoughtId) {
+				return;
+			}
+
 			const { error } = await supabase.from("collection_thoughts").insert({
 				collection_id: payload.collectionId,
 				thought_id: payload.thoughtId,
@@ -94,15 +125,13 @@ const useAddToCollection = () => {
 const useRemoveFromCollection = () => {
 	return useMutation({
 		mutationFn: async (payload: { collectionId: string; thoughtId: string }) => {
-			const { data, error } = await supabase
-				.from("collection_thoughts")
-				.delete()
-				.eq("collection_id", payload.collectionId)
-				.eq("thought_id", payload.thoughtId);
-
-			if (error) {
-				throw error;
-			}
+			handleSupabaseError(
+				await supabase
+					.from("collection_thoughts")
+					.delete()
+					.eq("collection_id", payload.collectionId)
+					.eq("thought_id", payload.thoughtId),
+			);
 
 			return payload.thoughtId;
 		},
@@ -110,20 +139,6 @@ const useRemoveFromCollection = () => {
 };
 
 export const CollectionCarousel = ({ thoughtId, collections }: { thoughtId?: string; collections: Collection[] }) => {
-	if (!thoughtId) {
-		return (
-			<div className="pointer-events-none opacity-60">
-				<Chip size="sm" variant="secondary" className="hover:bg-transparent">
-					<PlusIcon className="h-4 w-4" />
-					<span>Add to collection</span>
-				</Chip>
-			</div>
-		);
-	}
-	return <CollectionCarouselInner thoughtId={thoughtId} collections={collections} />;
-};
-
-const CollectionCarouselInner = ({ thoughtId, collections }: { thoughtId: string; collections: Collection[] }) => {
 	const { data: allCollections } = useCollections();
 
 	const { suggestedCollections, setSuggestedCollections } = useSuggestedCollectionsStore();
@@ -150,74 +165,76 @@ const CollectionCarouselInner = ({ thoughtId, collections }: { thoughtId: string
 	return (
 		<div className="w-screen -ml-6 pl-6 md:ml-0 md:pl-0 md:w-full overflow-x-auto no-scrollbar">
 			<div className="flex flex-nowrap gap-2 pb-2">
-				{collections?.map(collection => (
-					<Link key={collection.id} to={`/collections/${collection.id}`}>
-						<Chip
-							size="sm"
-							variant="secondary"
-							rightElements={
-								<div
-									className="h-full"
-									onClick={e => {
-										e.preventDefault();
-										e.stopPropagation();
-									}}>
-									<Dialog>
-										<DialogTrigger className="h-full appearance-none">
-											<Chip.Delete />
-										</DialogTrigger>
-										<DialogContent>
-											<DialogHeader>
-												<DialogTitle>Remove from collection</DialogTitle>
-											</DialogHeader>
-											<DialogDescription>
-												Are you sure you want to remove this thought from this collection?
-											</DialogDescription>
-											<DialogFooter>
-												<DialogCancel>Cancel</DialogCancel>
-												<DialogAction
-													className="bg-red-600 hover:bg-red-500"
-													onClick={() => {
-														removeFromCollection({
-															collectionId: collection.id,
-															thoughtId,
-														});
-													}}>
-													Remove
-												</DialogAction>
-											</DialogFooter>
-										</DialogContent>
-									</Dialog>
-								</div>
-							}>
-							<ListIcon className="h-4 w-4 flex-shrink-0" />
-							<span>{collection.title}</span>
-						</Chip>
-					</Link>
-				))}
-				{suggestedCollections.map(
-					suggestedCollection =>
-						!collectionIds.has(suggestedCollection.id) && (
+				{thoughtId &&
+					collections?.map(collection => (
+						<Link key={collection.id} to={`/collections/${collection.id}`}>
 							<Chip
-								key={suggestedCollection.id}
 								size="sm"
 								variant="secondary"
-								className="bg-accent/15"
-								onClick={() => {
-									addToCollection({
-										collectionId: suggestedCollection.id,
-										thoughtId,
-									});
-									setSuggestedCollections(
-										suggestedCollections.filter(collection => collection.id !== suggestedCollection.id),
-									);
-								}}>
-								<SparklesIcon className="h-3.5 w-3.5 flex-shrink-0 text-accent" />
-								<span>{"Add to "}</span>
-								<span className="font-bold">{suggestedCollection.title}</span>
+								rightElements={
+									<div
+										className="h-full"
+										onClick={e => {
+											e.preventDefault();
+											e.stopPropagation();
+										}}>
+										<Dialog>
+											<DialogTrigger className="h-full appearance-none">
+												<Chip.Delete />
+											</DialogTrigger>
+											<DialogContent>
+												<DialogHeader>
+													<DialogTitle>Remove from collection</DialogTitle>
+												</DialogHeader>
+												<DialogDescription>
+													Are you sure you want to remove this thought from this collection?
+												</DialogDescription>
+												<DialogFooter>
+													<DialogCancel>Cancel</DialogCancel>
+													<DialogAction
+														className="bg-red-600 hover:bg-red-500"
+														onClick={() => {
+															removeFromCollection({
+																collectionId: collection.id,
+																thoughtId,
+															});
+														}}>
+														Remove
+													</DialogAction>
+												</DialogFooter>
+											</DialogContent>
+										</Dialog>
+									</div>
+								}>
+								<ListIcon className="h-4 w-4 flex-shrink-0" />
+								<span>{collection.title}</span>
 							</Chip>
-						),
-				)}
+						</Link>
+					))}
+				{thoughtId &&
+					suggestedCollections.map(
+						suggestedCollection =>
+							!collectionIds.has(suggestedCollection.id) && (
+								<Chip
+									key={suggestedCollection.id}
+									size="sm"
+									variant="secondary"
+									className="bg-accent/15"
+									onClick={() => {
+										addToCollection({
+											collectionId: suggestedCollection.id,
+											thoughtId,
+										});
+										setSuggestedCollections(
+											suggestedCollections.filter(collection => collection.id !== suggestedCollection.id),
+										);
+									}}>
+									<SparklesIcon className="h-3.5 w-3.5 flex-shrink-0 text-accent" />
+									<span>{"Add to "}</span>
+									<span className="font-bold">{suggestedCollection.title}</span>
+								</Chip>
+							),
+					)}
 				<CollectionDropdown
 					trigger={
 						<Chip size="sm" variant="secondary">
