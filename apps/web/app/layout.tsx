ee3810551765d3d5047ec2d48fb38a0db816838c
jennmueng <ldrwarlock@gmail.com>
2024-09-04 19:21:26 -0700

    add posthog to web

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,28 +1,36 @@
-import { Noto_Sans, Red_Hat_Display } from "next/font/google"
+import dynamic from "next/dynamic";
+import { Noto_Sans, Red_Hat_Display } from "next/font/google";
+import "styles/tailwind.css";
 
-import "styles/tailwind.css"
-import { Navbar } from "./components/Navbar"
-import { Metadata } from "next"
+import { Navbar } from "./components/Navbar";
+import { PHProvider } from "./providers";
 
 const inter = Noto_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-sans",
-})
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-sans",
+});
 
 const redHatDisplay = Red_Hat_Display({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-display",
-})
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-display",
+});
+
+const PostHogPageView = dynamic(() => import("./PosthogPageView"), {
+	ssr: false,
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className={`${inter.variable} ${redHatDisplay.variable}`}>
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" className={`${inter.variable} ${redHatDisplay.variable}`}>
+			<PHProvider>
+				<body>
+					<PostHogPageView />
+					<Navbar />
+					{children}
+				</body>
+			</PHProvider>
+		</html>
+	);
 }
