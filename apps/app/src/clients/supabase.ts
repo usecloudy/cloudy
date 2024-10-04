ac65bea12e41b630ff4a5458fe9e5b0c5eb16087
jennmueng <ldrwarlock@gmail.com>
2024-10-03 21:21:20 -0700

    turn supabase autorefresh back on

--- apps/app/src/clients/supabase.ts
+++ apps/app/src/clients/supabase.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient<Database>(process.env.REACT_APP_SUPABASE_URL!, process.env.REACT_APP_SUPABASE_ANON_KEY!, {
 	auth: {
+		autoRefreshToken: true,
 		persistSession: true,
 	},
 });
