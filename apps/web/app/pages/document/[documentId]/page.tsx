b658f82135dacdb86d5b59c1c366a6b1a495108f
jennmueng <ldrwarlock@gmail.com>
2024-11-19 21:47:00 -0800

    fix bug

--- apps/web/app/pages/document/[documentId]/page.tsx
+++ apps/web/app/pages/document/[documentId]/page.tsx
@@ -6,31 +6,39 @@ import { DocumentView } from "./DocumentView";
 
 export const generateMetadata = async ({ params }: { params: { documentId: string } }) => {
 	const supabase = await getSupabaseAnonClient();
-	const document = handleSupabaseError(
-		await supabase.from("document_versions").select("id, title, content_md").eq("id", params.documentId).maybeSingle(),
+	const thought = handleSupabaseError(
+		await supabase
+			.from("thoughts")
+			.select("latest_version:document_versions!latest_version_id(id, title, content_md)")
+			.eq("id", params.documentId)
+			.eq("access_strategy", "public")
+			.maybeSingle(),
 	);
 
+	const documentVersion = thought?.latest_version && fixOneToOne(thought.latest_version);
+
 	return {
-		title: `${document?.title || "Untitled"} | Cloudy Pages`,
-		description: ellipsizeText(document?.content_md ?? "", 128),
+		title: `${documentVersion?.title || "Untitled"} | Cloudy Pages`,
+		description: ellipsizeText(documentVersion?.content_md ?? "", 128),
 	};
 };
 
 export default async function PublicDocumentPage({ params }: { params: { documentId: string } }) {
 	const supabase = await getSupabaseAnonClient();
-	let tmpDocumentVersion = handleSupabaseError(
+	const document = handleSupabaseError(
 		await supabase
 			.from("thoughts")
 			.select("latest_version:document_versions!latest_version_id(id, title, content_json)")
 			.eq("id", params.documentId)
+			.eq("access_strategy", "public")
 			.maybeSingle(),
-	)?.latest_version;
+	);
 
-	if (!tmpDocumentVersion) {
+	if (!document?.latest_version) {
 		return <div>Document not found or not public</div>;
 	}
 
-	const documentVersion = fixOneToOne(tmpDocumentVersion)!;
+	const documentVersion = fixOneToOne(document.latest_version)!;
 
 	return <DocumentView documentVersion={documentVersion} />;
 }
